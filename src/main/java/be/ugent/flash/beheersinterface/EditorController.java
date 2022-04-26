7b673ef5f743f7b6543c1408208ad219f5f90bc3
Thibaud Collyn <thibaud.collyn@ugent.be>
2022-04-26 10:11:23 +0200

    Refactored some files(clearly separated packages) and started work on part 2 of the project.

--- src/main/java/be/ugent/flash/beheersinterface/EditorController.java
+++ src/main/java/be/ugent/flash/beheersinterface/EditorController.java
@@ -3,8 +3,13 @@ package be.ugent.flash.beheersinterface;
 import java.io.File;
 
 public class EditorController extends StartScreenController {
-    private File selectedDB;
+    private final File selectedDB;
+
     public EditorController(File selectedDB) {
         this.selectedDB = selectedDB;
+    }
+
+    public void initialize(){
+        //TODO: implement
     }
 }
