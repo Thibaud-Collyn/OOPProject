ec8d03ec1f3594b750bd660d05d417940439aa9c
Thibaud Collyn <thibaud.collyn@ugent.be>
2022-05-03 09:06:11 +0200

    Small visual update to error message pop-up

--- src/main/java/be/ugent/flash/beheersinterface/parteditors/ErrorPopUp.java
+++ src/main/java/be/ugent/flash/beheersinterface/parteditors/ErrorPopUp.java
@@ -1,13 +1,19 @@
 package be.ugent.flash.beheersinterface.parteditors;
 
 import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -20,11 +26,14 @@ public class ErrorPopUp {
         Stage popUp = new Stage();
         popUp.setResizable(false);
         popUp.initModality(Modality.APPLICATION_MODAL);
-        box.setPrefSize(400, 100);
+        popUp.setTitle("ERROR");
+        box.setPrefSize(400, 90);
+        box.setSpacing(10);
         errorMessage.setText(error);
-        errorMessage.setTextFill(Color.RED);
-        errorMessage.setFont(Font.font(18));
-        ok.setFont(Font.font(14));
+        errorMessage.setTextFill(Color.WHITE);
+        errorMessage.setFont(Font.font(null, FontWeight.BOLD, 18));
+        errorMessage.setBackground(new Background(new BackgroundFill(Color.web("#ff6961"), new CornerRadii(2), Insets.EMPTY)));
+        ok.setFont(Font.font(null, FontWeight.BOLD, 14));
         ok.setOnAction(e -> popUp.close());
         box.getChildren().addAll(errorMessage, ok);
         box.setAlignment(Pos.CENTER);
